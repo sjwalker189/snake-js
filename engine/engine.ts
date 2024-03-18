76dd58209c1186170ea96ef34e9b12c40039ce9e
Sam Walker <sjwalker189@gmail.com>
2024-03-18 18:19:28 +1300

    wip

--- engine/engine.ts
+++ engine/engine.ts
@@ -8,49 +8,47 @@ enum Direction {
   RIGHT,
 }
 
-function randomInt(min: number, max: number) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-}
-
-function coordinateExists(items: Positions, row: number, col: number) {
-  return (
-    items.findIndex(
-      ([cellRow, cellCol]) => cellRow === row && cellCol === col,
-    ) >= 0
-  );
-}
 
 class EventEmitter {
+  listeners = new Map<string, (value: any) => void>();
+
   emit(msg: string, value: any) {
     //
   }
+
   on(msg: string, cb: (value: any) => void) {
-    //
+    if (this.listeners.has(msg)) {
+      this.listeners.get(msg)!.add(cb);
+    } else {
+      this.listeners.set(msg, new Set([cb]));
+    }
+
+    return () => {
+      this.listeners.get(msg)?.delete(cb);
+    }
   }
 }
 
 const AllowedMovements = new Map<Direction, Direction[]>([
-	[Direction.UP, [Direction.LEFT, Direction.RIGHT]],
-	[Direction.DOWN, [Direction.LEFT, Direction.RIGHT]],
-	[Direction.LEFT, [Direction.UP, Direction.DOWN]],
-	[Direction.RIGHT, [Direction.UP, Direction.DOWN]],
+  [Direction.UP, [Direction.LEFT, Direction.RIGHT]],
+  [Direction.DOWN, [Direction.LEFT, Direction.RIGHT]],
+  [Direction.LEFT, [Direction.UP, Direction.DOWN]],
+  [Direction.RIGHT, [Direction.UP, Direction.DOWN]],
 ]);
 
 export class GameEngine extends EventEmitter {
-	/** game controls */
+  /** game controls */
   start: number | undefined;
   end: number | undefined;
   tickRate = 500;
   options = {
     borders: true,
   };
-/** food */
+  /** food */
   consumed = 0;
   foodPosition: Coordinate = [-1, -1];
-  
-/** snake */
+
+  /** snake */
   currentDirection = Direction.UP;
   position: Positions = [
     [8, 16],
@@ -58,35 +56,44 @@ export class GameEngine extends EventEmitter {
     [8, 18],
   ];
 
-	constructor(
-		public rows = 16,
-		public cols = 32,
-	) {
-		super();
+  private locked = false;
 
-	}
+  constructor(
+    public rows = 16,
+    public cols = 32,
+  ) {
+    super();
+  }
+
+  private lock() {
+    this.locked = true;
+  }
+
+  private unlock() {
+    this.locked = false;
+  }
 
   get done() {
     return this.end !== undefined;
   }
 
-	getDirection(): Direction {
-		return this.currentDirection;
-	}
-	setDirection(newDirection: Direction) {
-		if (AllowedMovements.get(this.currentDirection)!.includes(newDirection)) {
-			this.currentDirection = newDirection;
-		}
-	}
- 
+  getDirection(): Direction {
+    return this.currentDirection;
+  }
+
+  setDirection(newDirection: Direction) {
+    if (this.locked) {
+      return;
+    }
+
+    if (AllowedMovements.get(this.currentDirection)!.includes(newDirection)) {
+      this.currentDirection = newDirection;
+    }
+  }
+
   clearFood() {
     this.foodPosition[0] = -1;
     this.foodPosition[1] = -1;
-    new EventEmitter();
-  }
-
-  randomCoordniate() {
-    return [];
   }
 
   placeFood() {
@@ -112,26 +119,51 @@ export class GameEngine extends EventEmitter {
 
   getNextCoordinate(): Coordinate | false {
     const [row, col] = this.position[0];
-    console.log(this.currentDirection, this.position[0]);
     let nextRow = row;
     let nextCol = col;
 
     switch (this.currentDirection) {
       case Direction.UP:
-        console.log("try move up");
         if (row === 1) {
           if (this.options.borders) {
-            console.log("hit border");
             return false;
           } else {
-            console.log("wrap around");
             nextRow = this.rows;
           }
         } else {
           nextRow = row - 1;
         }
+      case Direction.DOWN:
+        if (row === this.rows) {
+          if (this.options.borders) {
+            return false;
+          } else {
+            nextRow = 1;
+          }
+        } else {
+          nextRow = row + 1;
+        }
+      case Direction.LEFT:
+        if (col === 1) {
+          if (this.options.borders) {
+            return false;
+          } else {
+            nextCol = this.cols;
+          }
+        } else {
+          nextCol = col - 1;
+        }
+      case Direction.RIGHT:
+        if (col === this.cols) {
+          if (this.options.borders) {
+            return false;
+          } else {
+            nextCol = 1;
+          }
+        } else {
+          nextCol = col + 1;
+        }
     }
-    console.log([nextRow, nextCol]);
 
     if (coordinateExists(this.position, nextRow, nextCol)) {
       console.log("collided with self");
@@ -153,11 +185,7 @@ export class GameEngine extends EventEmitter {
 
     if (this.isFoodAvailable() && this.isFoodAt(row, col)) {
       console.log("Consumed food. Leveling up");
-      // Add the next position to the head ate static IEnumerable<int> numbers(int max) {
-        int n = 0;
-        while (n < max) {
-            yield return n++;
-        }to grow and move along the board
+      // Add the next position to the head to grow and move along the board
       this.position.unshift(next);
       this.consumed += 1;
       this.levelUp();
@@ -195,12 +223,36 @@ export class GameEngine extends EventEmitter {
     this.start = Date.now();
     this.setup();
     for await (const tick of this.loop()) {
-      console.log(`tick: ${tick}`);
+      console.log(`tick: ${tick}`, this.state());
+      this.lock();
       this.onTick();
+      this.unlock();
     }
+  }
+
+  private state() {
+    return {
+      snake: this.position,
+      consumed: this.consumed,
+    };
   }
 }
 
 function sleep(ms: number) {
   return new Promise((r) => setTimeout(r, ms));
+}
+
+
+function randomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function coordinateExists(items: Positions, row: number, col: number) {
+  return (
+    items.findIndex(
+      ([cellRow, cellCol]) => cellRow === row && cellCol === col,
+    ) >= 0
+  );
 }
